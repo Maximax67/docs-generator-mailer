6c0ecec9caf273a7fcc8d15c265cf572a3ad0ef9
Bielikov Maksym <maximax6767@gmail.com>
2025-10-06 18:45:24 +0300

    refactor: remove unused headers

--- app/main.py
+++ app/main.py
@@ -16,12 +16,6 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=[
-        "Retry-After",
-        "X-RateLimit-Reset",
-        "X-RateLimit-Limit",
-        "X-RateLimit-Remaining",
-    ],
 )
 
 
